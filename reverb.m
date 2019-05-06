0ccb35a83a7f6d0eee26449e5088296866343a61
Kevin <Murning>
2019-05-06 18:59:08 +0200

    adding all algo reverbs

--- reverb.m
+++ reverb.m
@@ -1,84 +1,72 @@
-%
-% EEE4114F PROJECT
-% Artificial Reverberation
-% 
-% Kevin Murning
-% Kelsey Kaplan
-%
-% May 2019
-%
-%
-%
-%
-%
+%%%%%%%%%%%%%%%%%%%%%%%
+% EEE4114F PROJECT                                     %
+% Artificial Reverberation                               %
+%                                                                             %
+% Kevin Murning                                                %
+% Kelsey Kaplan                                                 %
+%                                                                             %
+% May 2019                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%
+
 
 
 % load room impulse response
-a = audioinfo("GalbraithHall.wav"); 
-[ir,fs] = audioread("GalbraithHall.wav");
-%soundsc(ir, fs);
+a = audioinfo("SteinmanHall.wav"); 
+[ir,fs] = audioread("SteinmanHall.wav");
+
 
 
 % make single channel
 irMono = mean(ir, 2);
-%soundsc(irMono, fs);
+
 
 
 % room impulse response plot
 figure(1);
 plot(ir);
-%axis([0 1e4, -inf inf]) 
+
 title('Impulse Response of the Room');
 xlabel('Time [samples]')
 
 
-% room frequency spectrum plot
-%figure(2);
-%Y = fft(ir);
-%plot(abs(Y))
-N = fs; % number of FFT points
-transform = fft(ir,N)/N;
-magTransform = abs(transform);
-%faxis = linspace(-fs/2,fs/2,N);
-%plot(faxis,fftshift(magTransform));
-%xlabel('Frequency [Hz]')
-%axis([0 length(faxis)/2, 0 max(magTransform)])
-xt = get(gca,'XTick');  
-set(gca,'XTickLabel', sprintf('%.0f|',xt))
-
 
 % load anechoic (echoless) sound
-[x,fs2] = audioread("Anechoic.wav"); 
-%soundsc(x,fs2)
+[x,fs2] = audioread("drySpeech.wav"); 
+
+xmono = mean(x,2);
 
 % convolve with impulse response for idealised reverb - method 1
-y = conv(x,irMono); % convolution with FFT
-%soundsc(y,fs2)
+y = conv(xmono,irMono); % convolution with FFT
+soundsc(y,fs2)
 
 % plot convolved signal
-figure(3);
-plot(y,'b');
-hold on
-plot(x,'black');
-t = 0 : 1/fs : length(ir)/fs; % time point
-title('Impulse Response Reverberated Signal');
-xlabel('time'),ylabel('Amplitude')
-legend('Convolved Signal', 'Original Signal')
+
+
+% figure(3);
+% plot(y,'b');
+% hold on
+% plot(x,'black');
+% t = 0 : 1/fs : length(ir)/fs; % time point
+% title('Impulse Response Reverberated Signal');
+% xlabel('time'),ylabel('Amplitude')
+% legend('Convolved Signal', 'Original Signal')
 
 
 % convolve with impulse response for idealised reverb - method 2
 y2 = freqconv(x, irMono);
-soundsc(y2, fs2);
+%soundsc(y2, fs2);
 
 % plot convolved signal
-figure(4);
-plot(y2,'b');
-hold on
-plot(x,'black');
-t = 0 : 1/fs : length(ir)/fs; % time point
-title('Impulse Response Reverberated Signal');
-xlabel('time'),ylabel('Amplitude')
-legend('Convolved Signal', 'Original Signal')
+
+
+% figure(4);
+% plot(y2,'b');
+% hold on
+% plot(x,'black');
+% t = 0 : 1/fs : length(ir)/fs; % time point
+% title('Impulse Response Reverberated Signal');
+% xlabel('time'),ylabel('Amplitude')
+% legend('Convolved Signal', 'Original Signal')
 
 
 % function to convolve impulse response and anechoic sound
